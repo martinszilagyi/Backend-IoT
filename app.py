81d8e2d4b901296276668e61a22431830c02c981
martinszilagyi <szilagyimartin13@gmail.com>
2025-04-05 20:37:03 +0200

    minor updates

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+#Shameless, ugly, hard-coded API key...
 google_api_key = "AIzaSyC5TNTnmnxJ7Mkhx48--XOCSg7WX9NndHU"
 
+#This function decodes incoming data and 'jsonify' it to be compatible with Google Geolocation API.
 def json_for_Google_API(wifi_str):
     access_points = []
-    for line in wifi_str.strip().split('\n'): #separate each rows
-        parts = line.split('|')
+    for line in wifi_str.strip().split('\n'):                                   #separate each rows
+        parts = line.split('|')                                                 #separate each cols
         ssid = mac = rssi = None
         for part in parts:
             part = part.strip()
@@ -22,59 +24,66 @@ def json_for_Google_API(wifi_str):
             if (part.startswith("RSSI")):
                 rssi = part.split("RSSI:")[1].strip()
         if mac and rssi:
-            access_points.append({
+            access_points.append({                                              #temporary access point properites in json format
                 "macAddress": mac,
                 "signalStrength": rssi
             })
 
-    # Final payload for Google
+    #Final payload for Google
     google_payload = {
         "wifiAccessPoints": access_points
     }
 
+    #debug
     print(json.dumps(google_payload, indent=2))
 
     return google_payload
 
+#Rendering index.html
 @app.route('/')
 def index():
-    # Serve the HTML page directly from the backend
-    return render_template('index.html')  # This renders the HTML page when visiting "/"
+    return render_template('index.html')
 
+#upon triggering ttn-data
 @app.route('/ttn-data', methods=['POST'])
 def ttn_data():
     try:
-        # Get JSON payload from the request
+        #Get json payload from the request
         if request.is_json:
             ttn_payload = request.get_json()  # Parse JSON data
             #print('Received TTN Data:', ttn_payload)
 
-            # Check if the necessary fields exist in the data
+            #Check if the necessary fields exist in the data (Can be deleted...)
             if 'uplink_message' not in ttn_payload :
                 return jsonify({"error": "Missing required fields"}), 400
 
-            # Process data (you can add more functionality here as needed)
+            #Process data
             wifi_props = ttn_payload['uplink_message']['decoded_payload']['bytes']
+            #Decoding hex to text
             wifi_str = bytes(wifi_props).decode('utf-8')
 
+            #Create appropriate json structure for using Google geolocation API
             google_payload = json_for_Google_API(wifi_str)
 
+            #Invoke API with the json structure created
             url = f"https://www.googleapis.com/geolocation/v1/geolocate?key={google_api_key}"
             response = requests.post(url, json=google_payload)
             if response.status_code == 200:
-                # Process the JSON response
+                #Process the json response
                 data = response.json()
                 
-                # Extract and print relevant details
+                #Extract and print relevant details
                 if 'location' in data:
                     location = data['location']
                     latitude = location.get('lat')
                     longitude = location.get('lng')
                     accuracy = data.get('accuracy')
 
+                    #Debug
                     print(f"Latitude: {latitude}")
                     print(f"Longitude: {longitude}")
                     print(f"Accuracy: {accuracy} meters")
+
                     # Emit the data to all connected clients using the correct 'socketio.emit' method
                     socketio.emit('new_data', data)
                 else:
@@ -82,16 +91,17 @@ def ttn_data():
             else:
                 print(f"Error: {response.status_code} - {response.text}")
 
-            # Return a success response
+            #Return a success response
             return jsonify({"message": "Data received successfully", "data": ttn_payload}), 200
         else:
             return jsonify({"error": "Invalid data format. Expected JSON."}), 400
 
     except Exception as e:
-        # Log the error
+        #Log the error
         print(f"Error processing data: {e}")
         return jsonify({"error": f"Failed to process data: {str(e)}"}), 500
 
+#Start the application
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
