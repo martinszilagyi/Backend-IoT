041e948133637465cea99439264ead89fba6e041
martinszilagyi <szilagyimartin13@gmail.com>
2025-04-07 11:44:54 +0200

    No longer considering IP address

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def json_for_Google_API(wifi_str):
 
     #Final payload for Google
     google_payload = {
-        "wifiAccessPoints": access_points
+        "wifiAccessPoints": access_points,
+        "considerIp": False
     }
 
     #debug
