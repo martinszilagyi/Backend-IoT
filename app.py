e5c4c77fdf9508082cff0b32116f6891e727088b
martinszilagyi <szilagyimartin13@gmail.com>
2025-05-17 09:47:16 +0200

    port cahnged to 6

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def json_for_Google_API(wifi_bytes):
     #Returns with all extracted data
     return status_str, percentage, google_payload
 
-def send_downlink(app_id, device_id, payload_bytes, f_port=1):
+def send_downlink(app_id, device_id, payload_bytes, f_port=6):
     url = f"https://eu1.cloud.thethings.network/api/v3/as/applications/{app_id}/devices/{device_id}/down/push"
 
     auth_key = "Bearer " + ttn_api_key
